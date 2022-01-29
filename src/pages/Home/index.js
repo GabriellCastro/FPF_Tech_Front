a18ce2e036fd7889f2d46c844c892134d8055947
Gabriel Castro <kastro.gabriel17@gmail.com>
2022-01-29 15:55:59 -0400

    Erro de versao do react-router-dom corrigido! 🚀

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,6 +4,7 @@ function Home() {
   return ( 
     <>
       <h1>Home</h1>
+      { console.log("aqui") }
     </>
    );
 }
--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -5,23 +5,21 @@ import Home from '../pages/Home';
 import ProjectNew from '../pages/ProjectNew';
 import ProjectEdit from '../pages/ProjectEdit';
 
-function Routes() {
-  return ( 
-    <BrowserRouter>
-      <Switch>
-        <Route
-          exact
-          path="/"
-          component={ Home } />
-        <Route
-          path="/new"
-          component={ ProjectNew } />
-        <Route
-          path="/edit/:id"
-          component={ ProjectEdit } />
-      </Switch>
-    </BrowserRouter>
-   );
-}
+const Routes = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route
+        exact
+        path="/"
+        component={ Home } />
+      <Route
+        path="/new"
+        component={ ProjectNew } />
+      <Route
+        path="/edit/:id"
+        component={ ProjectEdit } />
+    </Switch>
+  </BrowserRouter>
+)
 
 export default Routes;
